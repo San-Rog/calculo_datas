90801a2936372c187e9932e9581a920e5f3b142b
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-13 18:08:37 -0300

    Update calDaysUsefulNew.py

--- calDaysUsefulNew.py
+++ calDaysUsefulNew.py
@@ -298,10 +298,10 @@ def iniVars():
             
 def checkDate(dateSel, nDays):
     if nDays <= 0:
-        st.toast("⚠️ e os gráficos não exibirão dados representativos da contagem!")
-        time.sleep(0.2)
-        st.toast(f"⚠️ A rotina baseou-se em {nDays} dia {sing}, de modo que as tabelas (junto com os arquivos de download) ")
-        time.sleep(0.2)
+        msg = st.toast("⚠️ e os gráficos não exibirão dados representativos da contagem!")
+        time.sleep(0.1)
+        msg.toast(f"⚠️ A rotina baseou-se em {nDays} dia {sing}, de modo que as tabelas (junto com os arquivos de download) ")
+        time.sleep(0.1)
         
 def main():
     global output, dirRoot
