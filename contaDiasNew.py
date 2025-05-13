d82d8d8ade65f8b457fce19fbb574007117eaf86
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-13 07:02:23 -0300

    Update contaDiasNew.py

--- contaDiasNew.py
+++ contaDiasNew.py
@@ -16,6 +16,7 @@ def dateFullLang(date):
     return dateFull
 
 def findCurFul():
+    st.write(st.session_state.dateonly)
     colorIni = st.session_state.color
     @st.dialog(' ')
     def config():
