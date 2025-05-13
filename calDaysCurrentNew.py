fab3655b834777a0ecabed6fd789de8710af94f7
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-13 08:52:45 -0300

    Update calDaysCurrentNew.py

--- calDaysCurrentNew.py
+++ calDaysCurrentNew.py
@@ -338,7 +338,7 @@ def main():
             dfCount = treatmentDf(title, field)
             st.dataframe(data=dfCount, hide_index=True, use_container_width=True)
             chartData = graphicDf(title)
-            st.bar_chart(chartData, y="frequência", x=field)        
+            st.bar_chart(chartData, y="frequência", x=field)      
         output = BytesIO() 
     iniVars()
 
