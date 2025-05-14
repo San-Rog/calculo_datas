6338460a550386ec5b74a6c7e68f211eb4c1eb4a
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-14 15:42:23 -0300

    Update contaDiasNew.py

--- contaDiasNew.py
+++ contaDiasNew.py
@@ -220,8 +220,8 @@ def main():
         Esta aplicação permite calcular prazos em dias corridos ou dias úteis. Parte de data data e números de dias
         de interesse do usuário. No caso do período em dias corridos, entram sábados, domingos e feriados, desde que 
         nenhum deles esteja no início ou término da contagem. Já na contagem em dias úteis, sábados, domingos e feriados
-        não entram, esteja no meio ou nas datas extremas (começo ou final) da contagem.
-        Esta ferramenta não é um aplicativo rigoroso ou oficial, especialmente porque não leva em consideração datas em que, 
+        não entram, estejam no meio ou nas datas extremas (começo ou final) da contagem.
+        Esta ferramenta não representa um aplicativo rigoroso ou oficial, especialmente porque não leva em consideração datas em que, 
         por diferentes motivos, não houver expediente público ou privado. 
         Mesmo em relação aos feriados nacionais, chama-se a atenção para o fato de que se baseiam em planilha disponível 
         na internet e copiada em 13 de maio de 2025 pelo desenvolvedor.                      
