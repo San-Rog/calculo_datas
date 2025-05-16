09220dd88849f477436f499f10728d206b44970c
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-16 10:08:14 -0300

    Update calDaysUsefulNew.py

--- calDaysUsefulNew.py
+++ calDaysUsefulNew.py
@@ -88,7 +88,7 @@ def countCurUseFul(dateTuple):
                         else:
                             status = 'conta'
                             obs = 'dia normal'
-                            count += 1
+                    count += 1
             else:
                 if any ([weekNum == 5 or weekNum == 6]):
                     status = 'não conta'
@@ -328,7 +328,7 @@ def main():
     dateNow = datetime.date.today()
     dayFirst = st.session_state['acesso'][0]
     nDays = st.session_state['acesso'][1]
-    arg = (dayFirst, nDays, 1, f'contagem em dias {plur}', 'Demonstrativo cronológico')
+    arg = (dayFirst, nDays, 0, f'contagem em dias {plur}', 'Demonstrativo cronológico')
     countCurUseFul(arg)
     df = pd.DataFrame(dateCurrUse)
     #['dia do mês', 'dias da semana', 
