42d53eddc96e152906e1f2bb4c81eb1fa73474e3
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-16 14:03:08 -0300

    Update calDaysCurrentNew.py

--- calDaysCurrentNew.py
+++ calDaysCurrentNew.py
@@ -158,7 +158,7 @@ def graphicDf(title):
     link = 'https://en.wikipedia.org/wiki/Decimal_separator'
     colEstat.markdown(f":bar_chart: **<font color={color}>{title}</font>**", True)    
     st.write(dfCount['frequência'].tolist())
-    colUrl.markdown(f":material/emergency{link}", True)
+    colUrl.markdown(f"{:material/emergency}{link}", True)
     return chartData
     
 def toCsv():
