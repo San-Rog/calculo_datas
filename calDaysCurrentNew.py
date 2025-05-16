f10b2453df0cc57a1b2a4d0b2f28f0755e885921
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-16 13:30:44 -0300

    Update calDaysCurrentNew.py

--- calDaysCurrentNew.py
+++ calDaysCurrentNew.py
@@ -155,7 +155,8 @@ def graphicDf(title):
     colEmpty, = st.columns(spec=1, gap='small', vertical_alignment='top')
     colEmpty.text('')
     colEstat, = st.columns(spec=1, gap='small', vertical_alignment='top')
-    colEstat.markdown(f":bar_chart: **<font color={color}>{title}</font>**", True)    
+    link = 'www.tcu.jus.br'
+    colEstat.markdown(f":bar_chart: **<font color={color}>{title}</font>**{link}", True)    
     return chartData
     
 def toCsv():
