e0de0704acebad1f2068a716b1546f12229af385
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-19 23:33:01 -0300

    Update calDaysCurrentNew.py

--- calDaysCurrentNew.py
+++ calDaysCurrentNew.py
@@ -340,7 +340,7 @@ def main():
     dateNow = datetime.date.today()
     dayFirst = st.session_state['acesso'][0]
     nDays = st.session_state['acesso'][1]
-    arg = (dayFirst, nDays, 1, f'contagem em dias {plur}', 'Demonstrativo cronológico')
+    arg = (dayFirst, nDays, 0, f'contagem em dias {plur}', 'Demonstrativo cronológico')
     countCurUseFul(arg)
     df = pd.DataFrame(dateCurrUse)
     #['dia do mês', 'dias da semana', 
@@ -362,7 +362,8 @@ def main():
         st.dataframe(data=dfCount, hide_index=True, use_container_width=True)
         title = f"Gráfico '{field} x frequência' no período da contagem"        
         chartData = graphicDf(title)
-        st.bar_chart(chartData, y="frequência", x=field)    
+        with st.container(border=True):
+            st.bar_chart(chartData, y="frequência", x=field)    
         output = BytesIO()
     iniVars()
 
