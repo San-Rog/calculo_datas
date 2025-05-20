6b87a075d77304eda16fe15c3712f1e734d7e931
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 00:16:39 -0300

    Update contaDiasNew.py

--- contaDiasNew.py
+++ contaDiasNew.py
@@ -208,7 +208,7 @@ def main():
                                          """)
         nSlDate = sldDate.slider(label='Incremento de data', min_value=incMin, max_value=incMax, value=0, 
                                  key=listKeys[5], step=1, on_change=changeSlCalend, label_visibility="hidden")                         
-        nDays = colDays.number_input(label='Número de dias', step=1, key=listKeys[1], 
+        nDays = colDays.number_input(label='Número de dias', min_value=0, step=1, key=listKeys[1], 
                                      help="""
                                      Digite o número de dias da contagem. Se quiser incrementá-lo, pressione o símbolo '+' ou 
                                      mova o botão deslizante para a direita; se quiser decrementá-lo, pressione o símbolo '-' ou 
