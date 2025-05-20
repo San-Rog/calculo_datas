35817fa54fb52fe48aba07052facf09651a945b3
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 20:25:50 -0300

    Update contaDiasNew.py

--- contaDiasNew.py
+++ contaDiasNew.py
@@ -247,7 +247,7 @@ def main():
             css = f.read()
         st.markdown(f'<style>{css}</style>', unsafe_allow_html=True) 
         st.markdown("""<style> [data-testid="stDateInput"] [data-baseweb="input"]:before {
-                    content: url(https://i.imgur.com/pIZPHar.jpg);
+                    content: '   ' + url(https://i.imgur.com/pIZPHar.jpg);
                     padding-top: 5px;
                     padding-left: 7px;
                     text-align: right;
